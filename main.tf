846712b1de4ba7ca8cff054e605ae5f266e7249f
Oleksii Kalinin <oleksiikalinin@pingidentity.com>
2023-11-07 12:25:59 +0100

    Enable versioning

--- main.tf
+++ main.tf
@@ -12,14 +12,21 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-resource "aws_s3_bucket_acl" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl = "private"
-}
-
 resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "${random_string.s3_bucket_name.id}-lock"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
